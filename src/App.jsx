45130c1104054cc7c22937e4c97a3eb92b9be917
mukhsia <andre.n.mukhsia@gmail.com>
2023-12-05 16:41:34 -0800

    Added MovieInfo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import MovieInfo from "./components/MovieInfo/MovieInfo";
 import MovieList from "./components/MovieList/MovieList";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <MovieList />
+      <MovieInfo title="Forrest Gump" runningTime="142" genre="Romance" />
     </div>
   );
 }
