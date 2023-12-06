c64390b7b6ddcbd6b810fe5a891f2adfdf4ea812
mukhsia <andre.n.mukhsia@gmail.com>
2023-12-05 17:30:59 -0800

    Lifting movies state.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Header from "./components/Header/Header";
 import MovieInfo from "./components/MovieInfo/MovieInfo";
 import MovieList from "./components/MovieList/MovieList";
 import NewMovieForm from "./components/NewMovieForm/NewMovieForm";
+import React, { useState } from "react";
 
 function App() {
+    const [movies, setMovies] = useState([]);
     const selectedMovie = {
         title: "Forrest Bump",
         runningTime: 142,
@@ -15,7 +17,7 @@ function App() {
         <div className="App">
             <Header />
             <div className="flex-container">
-                <MovieList />
+                <MovieList movies={movies} />
                 <MovieInfo movieObj={selectedMovie} />
                 <NewMovieForm />
             </div>
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,17 +1,16 @@
 import MovieItem from "../MovieItem/MovieItem";
 
-const MovieList = ({}) => {
-  const movies = ["Princess Bride", "Forrest Gump", "Iron Man", "Pulp Fiction"];
-  const movieItems = movies.map((movie) => (
-    <MovieItem key={movie} title={movie} />
-  ));
+const MovieList = ({ movies = [] }) => {
+    const movieItems = movies.map((movie) => (
+        <MovieItem key={movie} title={movie} />
+    ));
 
-  return (
-    <div className="flex-item">
-      <h4>My Movies</h4>
-      <div>{movieItems}</div>
-    </div>
-  );
+    return (
+        <div className="flex-item">
+            <h4>My Movies</h4>
+            <div>{movieItems}</div>
+        </div>
+    );
 };
 
 export default MovieList;
